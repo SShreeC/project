c4bf2901580cbb5c51d439a15dc4567e64282288
SShreeC <123401235+SShreeC@users.noreply.github.com>
2024-10-03 14:02:41 +0530

    Delete src directory

--- src/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const cors=require("cors");
-const {login}=require("../controllers/login");
-
-const router=express.Router();
-router.use(cors());
-router.post("/login",login);
-
-module.exports=router;
